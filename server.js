156bca446e9ef2362689d1fd623f3506e03fa9a0
Léo <cdf.leo.merkel@gmail.com>
2026-09-16 21:03:48 +0200

    huuge commit add s3 connexion, upload video and authentifiaction

--- server.js
+++ server.js
@@ -3,25 +3,44 @@ import multer from 'multer';
 import cors from 'cors';
 
 import { createServer } from 'node:http';
-import { fileURLToPath } from 'node:url';
-import {dirname, join} from 'node:path';
 import { Server } from 'socket.io';
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'videos/');
+import multerS3 from 'multer-s3';
+import {GetObjectCommand, S3Client} from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+
+const s3 = new S3Client({
+    endpoint: `${process.env.S3_IP}:${process.env.S3_PORT}`,
+    region: 'us-east-1',
+    credentials: {
+        accessKeyId: process.env.S3_ACCESS,
+        secretAccessKey: process.env.S3_SECRET
     },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname);
+    forcePathStyle: true
+});
+
+const upload = multer({
+    storage: multerS3({
+        s3: s3,
+        bucket: process.env.S3_BUCKET_NAME,
+        key: function (req, file, cb) {
+            cb(null, file.originalname);
+        }
+    }),
+    fileFilter: function (req, file, cb) {
+        if (file.mimetype.startsWith('video/')) {
+            cb(null, true);
+        } else {
+            cb(new Error('Invalid file type'), false);
+        }
+    },
+    limits: {
+        fileSize: 1024 * 1024 * 50
     }
 })
-const upload = multer({ storage: storage })
 
 const app = express();
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
 app.use(cors({ origin: '*' }));
-app.use('/videos', express.static(join(__dirname, 'videos')));
 
 const server = createServer(app);
 const io = new Server(server, {
@@ -31,17 +50,75 @@ const io = new Server(server, {
     }
 });
 
-app.post('/videos-upload', upload.single("video"), function (req, res) {
-    if (!req.file) {
-        return res.status(400).json({ error: "Aucun fichier reçu" });
+io.use((socket, next) => {
+    const password = socket.handshake.auth.token
+
+    if (password === process.env.GLOBAL_PASSWORD) {
+        return next();
     }
-    console.log("Fichier sauvegardé :", req.file.filename);
-    res.status(201).json({ success: "Created", videoName: req.file.filename });
+    return next(new Error('Acces Refusé'));
+})
+
+const checkAuth = (req, res, next) => {
+    const clientPass = req.headers['authorization']
+
+    if (clientPass !== process.env.GLOBAL_TOKEN) {
+        return res.status(401).json({
+            error : 'Mot de passe incorrect'
+        })
+    }
+    next();
+}
+
+app.post('/videos-upload', checkAuth, function (req, res) {
+    const uploadSingle = upload.single("video");
+
+    uploadSingle(req, res, function (err) {
+        const videoText = req.body.name
+        if (videoText.length >= 31) {
+            return res.status(400).json({
+                error: 'Text trop long'
+            });
+        }
+        if (err) {
+            if (err.message === 'Invalid file type') {
+                return res.status(415).json({
+                    error: "Format non supporté. Seules les vidéos sont acceptées."
+                });
+            }
+            if (err.code === 'LIMIT_FILE_SIZE') {
+                return res.status(413).json({
+                    error: "Le fichier est trop volumineux."
+                });
+            }
+            return res.status(500).json({ error: "Erreur lors de l'upload du fichier." });
+        }
+        if (!req.file) {
+            return res.status(400).json({ error: "Aucun fichier reçu" });
+        }
+
+        console.log("Fichier sauvegardé :", req.file.key);
+        res.status(201).json({ success: "Created", videoName: req.file.key });
+    });
 });
 
+app.get('/videos/:id', checkAuth, async (req, res) => {
+    try {
+        const videoId = req.params.id;
+        const command = new GetObjectCommand({
+            Bucket: process.env.S3_BUCKET_NAME,
+            Key: videoId
+        })
+        const url = await getSignedUrl(s3, command, { expiresIn: 3600 })
+        res.json({ url:url })
+    } catch (error) {
+        console.error("Erreur de la génération du lien : ", error);
+        res.status(500).json({ error: "Impossible de récupérer la vidéo"})
+    }
+})
 io.on('connection', (socket) => {
     socket.on('newChat', (msg) => {
-        console.log("video envoyé.. lecture", msg)
+        console.log("Vidéo Emis", msg)
         io.emit('sendChat', msg)
     });
 });
